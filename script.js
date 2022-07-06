9a6ae10cc0633b9198684e5076675da78eecfcd9
Santana Fernandez <santanamcf01@gmail.com>
2022-07-05 18:09:41 -0700

    created functionality for localstorage targeting each textarea

--- script.js
+++ script.js
@@ -1,38 +1,50 @@
-$(document).ready(function(){   
+$(document).ready(function () {
 
 
     var currentHour = moment().hours();
     console.log(typeof currentHour)
 
-    $( ".time-block" ).each(function( index ) {
+    $(".time-block").each(function (index) {
         var timeBlockHour = parseInt($(this).attr("id"))
-        console.log(typeof timeBlockHour );
-        if (currentHour > timeBlockHour){
+        console.log(typeof timeBlockHour);
+        if (currentHour > timeBlockHour) {
             $(this).addClass("past")
-        
+
         }
-        else if(currentHour === timeBlockHour){
+        else if (currentHour === timeBlockHour) {
             $(this).addClass("present")
         }
-        else{
+        else {
             $(this).addClass("future")
         }
+
+        var now = moment().format("MMM Do, YYYY");
+        $("#currentDay").text(now);
+
+    
+// creating function to set and get items from localstorage.
+        function retrieveItem(button, textarea, key) {
+            $(button).on('click', function () {
+                //storing value of textarea within variable.
+                var setValue = $(textarea).val();
+                //setting value of text area to localstorage
+                localStorage.setItem(key, setValue)
+            })
+//using localstorage.get item to get value paired with key.
+            var getValue = localStorage.getItem(key)
+            $(textarea).val(getValue)
+        }
+        // calling function for each timeblock.
+        retrieveItem(".saveBtn", ".description9", "key9")
+        retrieveItem(".saveBtn", ".description10", "key10")
+        retrieveItem(".saveBtn", ".description11", "key11")
+        retrieveItem(".saveBtn", ".description12", "key12")
+        retrieveItem(".saveBtn", ".description13", "key13")
+        retrieveItem(".saveBtn", ".description14", "key14")
+        retrieveItem(".saveBtn", ".description15", "key15")
+        retrieveItem(".saveBtn", ".description16", "key16")
+         retrieveItem(".saveBtn", ".description17", "key17")
+         retrieveItem(".saveBtn", ".description18", "key18")
         
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     });
-
-      
-
-
-
 })
